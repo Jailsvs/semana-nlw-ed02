14b37ef7eac14990553d254366e23f78bae68367
jailsvs <jailsvs@gmail.com>
2020-08-08 10:58:23 -0300

    Web - Landing Page

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import "./assets/styles/global.css";
+import Landing from './pages/landing'
+
+function App() {
+  return (
+    <div className="App">
+      <Landing/>
+    </div>
+  );
+}
+
+export default App;
