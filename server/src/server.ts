6fc3797af53785a14359d584bba790ae0fc9239e
jailsvs <jailsvs@gmail.com>
2020-08-15 13:18:24 -0300

    commir server

---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+import express, { json } from 'express';
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+//Route Params: Qual recurso atualizar/deletar - request.params
+//Query Params: Paginação, Filtros, Ordenação, etc - request.query
+
+/*app.get('/', (request, response) => {
+  return response.json({message: "Server"});
+});*/
+
+
+app.listen(3333);
